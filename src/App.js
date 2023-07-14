e8e987bdf47b30cd63f734c97063bb12ac9406a0
Subeom Kwon <subeomkwon@gmail.com>
2023-07-14 13:35:45 +0900

    Implement Google Sign In Button

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import GoogleSignIn from "./components/GoogleSignIn";
+import styles from "./components/Button.module.css";
+import { useState } from "react";
 
 function App() {
+
+  const [user, setUser] = useState({});
+
+  const handleImageError = (e) => {
+    e.target.onerror = null; // Prevent infinite loop if the default image URL also fails
+    e.target.src = "https://path/to/your/default/image.png";
+  };
+
+  function handleSignOut(event) {
+    setUser({});
+    document.getElementById("signInDiv").hidden = false;
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div style={{ display: "flex", top: "10px", left: "200 px" }}>
+        <GoogleSignIn setUser={setUser} />
+
+        <div className="App">
+          {user && (
+            <div style={{ position: "absolute", top: "10px", right: "10px" }}>
+              <div className={`${styles["user-info"]}`}>
+                <img
+                  src={user.picture}
+                  onError={handleImageError}
+                  style={{ width: "50px", borderRadius: "50%" }}
+                ></img>
+                <h3 style={{ margin: "0" }}>{user.name}</h3>
+                {Object.keys(user).length !== 0 && (
+                  <button
+                    className={styles.button}
+                    onClick={(e) => {
+                      handleSignOut(e);
+                      // resetSession();
+                    }}
+                  >
+                    Sign Out
+                  </button>
+                )}
+              </div>
+            </div>
+          )}
+        </div>
+        </div>
   );
 }
 
