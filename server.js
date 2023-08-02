416a2526bdbdeeaaef5d0810212bb1af9ed71c93
Subeom Kwon <subeomkwon@gmail.com>
2023-08-02 14:41:31 +0900

    Connected to MongoDB Atlas

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
 const cors = require('cors');
+const mongoose = require('mongoose');
 const app = express();
-
 
 require('dotenv').config();
 
@@ -10,11 +10,51 @@ const port = 5000;
 
 const connectionString = process.env.MONGO_CONNECTION
 
+// Mongoose connection
+mongoose.connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch((error) => console.error('Could not connect to MongoDB: ', error));
+
+// User schema and model
+const { Schema, model } = mongoose;
+
+const userSchema = new Schema({
+  email: String,
+  categories: [String]
+});
+
+const User = model('User', userSchema);
+
 app.use(cors());
 app.use(express.json()); // Built-in middleware to parse incoming json
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+});
+
+app.post('/saveUser', async (req, res) => {
+  const { email, categories } = req.body;
+
+  // Data validation
+  if (typeof email !== 'string' || !Array.isArray(categories) || !categories.every(c => typeof c === 'string')) {
+    return res.status(400).send('Invalid data format');
+  }
+
+  try {
+    await User.findOneAndUpdate(
+      { email },
+      { email, categories },
+      { upsert: true, new: true, setDefaultsOnInsert: true },
+    );
+
+    res.send('User saved!');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error saving user');
+  }
 });
 
 app.post('/sendEmail', async (req, res) => {
