a8430328a41b5c7717bdd9989576e67f85764371
Subeom Kwon <subeomkwon@gmail.com>
2023-08-07 12:53:25 +0900

    Update placeholder message to be categories elements in database

--- server.js
+++ server.js
@@ -60,6 +60,19 @@ app.post('/saveUser', async (req, res) => {
 app.post('/sendEmail', async (req, res) => {
   const userEmail = req.body.userEmail;
 
+  let user;
+  try {
+    user = await User.findOne({ email: userEmail });
+    if (!user) {
+      return res.status(404).send('User not found');
+    }
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send('Error fetching user');
+  }
+
+  const categories = user.categories.join('\n');  // Joining categories with new line
+
   let transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
@@ -68,13 +81,19 @@ app.post('/sendEmail', async (req, res) => {
     }
   });
 
-  let info = await transporter.sendMail({
-    from: '"SK" <subeomkwon@gmail.com>', // sender address
-    to: userEmail, // list of receivers
-    subject: "Hello ✔", // Subject line
-    text: "Hello world?", // plain text body
-    html: "<b>Hello world?</b>", // html body
-  });
+  let info;
+  try {
+    info = await transporter.sendMail({
+      from: '"SK" <subeomkwon@gmail.com>', // sender address
+      to: userEmail, // list of receivers
+      subject: "Your Subscribed Categories", // Subject line
+      text: categories, // Sending categories in plain text
+      html: `<b>${categories.replace(/\n/g, '<br>')}</b>`, // Replacing new lines with <br> for HTML version
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send('Error sending email');
+  }
 
   res.send('Email sent!');
 });
