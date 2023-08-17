9401b84c60e00acd25556d5119eeef90f7399872
Subeom Kwon <subeomkwon@gmail.com>
2023-08-17 10:35:22 +0900

    Implement web crawler to crawl hallyu related articles

--- crawler.py
+++ crawler.py
@@ -1,36 +1,84 @@
-# import requests
-# from bs4 import BeautifulSoup
+import os
+import re
+import requests
+from bs4 import BeautifulSoup
+from collections import deque
+from urllib.parse import urljoin
+import zipfile
 
-# def get_articles():
-#     headers = {
-#         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
-#     }
+# Set seed URLs and keywords
+seed_urls = [
+    "https://www.google.com/search?sca_esv=557654684&sxsrf=AB5stBj5VHazSwMlDsOFwe1WNrVHW35bfQ:1692235901197&q=hallyu&tbm=nws&source=lnms&sa=X&sqi=2&ved=2ahUKEwjwiJH-xeKAAxWPa_UHHVaHDOgQ0pQJegQIDRAB&biw=2752&bih=1035&dpr=1.25",
+]
+keywords = ["Korean entertainment", "K-pop", "Hallyu"]
 
-#     url = "https://www.google.com/search?q=k-pop+OR+hallyu+OR+korean+wave+OR+korean+entertainment&source=lnms&tbm=nws"
-#     response = requests.get(url, headers=headers)   
-#     print(response.status_code)
-#     print(response.text[:1000])  # print the first 1000 characters of the response
+# Function to filter relevant links
+def is_relevant(url, text):
+    text = text.lower()
+    keyword_count = sum([1 for kw in keywords if kw.lower() in text])
+    return keyword_count > 0
 
-#     if response.status_code != 200:
-#         print("Failed to retrieve the web page.")
-#         return []
+# Function to score a URL based on keyword matches
+def score_url(text):
+    text = text.lower()
+    keyword_count = sum([1 for kw in keywords if kw.lower() in text])
+    return keyword_count / len(keywords)
 
-#     soup = BeautifulSoup(response.text, 'html.parser')
-#     articles = []
+# Web crawling loop
+visited = set()
+queue = deque([(url, 1) for url in seed_urls])
+output_folder = "collected_webpages"
 
-#     for node in soup.select('.tF2Cxc')[:10]:  # limit to first 10
-#         title = node.select_one('h3').text
-#         link = node.select_one('.yuRUbf a')['href']
-#         snippet = node.select_one('.Y3v8qd').text
-#         articles.append({
-#             "title": title,
-#             "link": link,
-#             "snippet": snippet
-#         })
+if not os.path.exists(output_folder):
+    os.mkdir(output_folder)
 
-#     return articles
+count = 1
+while queue and count <= 10:
+    url, _ = queue.popleft()
 
-# if __name__ == "__main__":
-#     articles = get_articles()
-#     for article in articles:
-#         print(article)
+    if url in visited:
+        continue
+
+    try:
+        response = requests.get(url)
+    except requests.RequestException as e:
+        print(f"Error downloading {url}: {e}")
+        continue
+
+    visited.add(url)
+
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, "html.parser")
+        links = soup.find_all("a")
+
+        for link in links:
+            href = link.get("href")
+            if href and not href.startswith("javascript:") and not href.startswith("#"):
+                absolute_url = urljoin(url, href)
+                if absolute_url not in visited:
+                    if is_relevant(absolute_url, link.text):
+                        queue.append((absolute_url, score_url(link.text)))
+
+        file_path = os.path.join(output_folder, f"Hallyu_{count:01}.html")
+        with open(file_path, "w", encoding="utf-8") as file:
+            file.write(response.text)
+
+        print(f"Downloaded {url} as {file_path}")
+
+        count += 1
+
+# Save the list of visited URLs
+with open("url_lists.txt", "w", encoding="utf-8") as file:
+    for url in visited:
+        file.write(url + "\n")
+
+print("Web crawling complete.")
+
+def zipdir(path, ziph):
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            ziph.write(os.path.join(root, file), os.path.relpath(os.path.join(root, file), path))
+
+with zipfile.ZipFile('webpages.zip', 'w', zipfile.ZIP_DEFLATED) as zipf:
+    zipdir('collected_webpages', zipf)
+    zipf.write('url_lists.txt')
